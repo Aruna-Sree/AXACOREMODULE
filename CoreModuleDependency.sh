8c283424889c877df17909e568d59e680a7d28a6
ArunaYarra <aruna.yarra@broadcom.com>
2022-01-20 21:26:32 +0530

    deleted unwanted files

--- CoreModuleDependency.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# This script will add the OTHER_LDFLAGS in build.xcconfig
-
-
-PROJECT_DIR=''
-
-camdoReporterHeader=$(find -L ${PODS_ROOT}/Headers/Public/CAMobileAppAnalytics \
--type f -iname “CAMDOReporter.h”)
-echo "File found $camdoReporterHeader"
